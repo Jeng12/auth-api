b0c718bdcbf106fef01c79ecc3b5f44546c6d40b
Jeng12 <70948730+Jeng12@users.noreply.github.com>
2026-05-17 08:58:35 +0700

    Temporary: check env vars and .env file existence

--- api/index.php
+++ api/index.php
@@ -1,17 +1,9 @@
 <?php
-
-define('LARAVEL_START', microtime(true));
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+header('Content-Type: application/json');
+echo json_encode([
+    'php' => PHP_VERSION,
+    'env_file_exists' => file_exists(__DIR__ . '/../.env'),
+    'app_key_set' => !empty(getenv('APP_KEY')),
+    'db_url_set' => !empty(getenv('DB_URL')),
+    'app_env' => getenv('APP_ENV') ?: 'not set',
+]);
